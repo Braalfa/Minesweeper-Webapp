2a2620e5143a69ee24ac6cb9fec5bea64d142613
Brayan Alfaro G <alfabrayan12@gmail.com>
2022-05-28 12:45:02 -0600

    Revert "Gitignore"
    This reverts commit 435347d1dfd9b1d762c7148e101d069b996cde6b.

---
+++ BuscaminasAPI/Modelos/Partida.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace BuscaminasAPI.Modelos
+{
+    public class Partida
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.Int32)]
+        public long id { get; set; }
+        public int altura { get; set; }
+        public int anchura { get; set; }
+        public string email { get; set; }
+        public int minas { get; set; }
+        public DateTime fechaCreacion { get; set; }
+        public int [] tableroPartida { get; set; }
+        public int[] tableroJugador { get; set; }
+        public string estado { get; set; }
+
+    }
+}
