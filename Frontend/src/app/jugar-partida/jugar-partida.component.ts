c24563525a75a93bfef54403a1dd5208852555a5
Brayan Alfaro G <alfabrayan12@gmail.com>
2022-06-05 01:06:36 -0600

    Brayitan request for quick flags

--- Frontend/src/app/jugar-partida/jugar-partida.component.ts
+++ Frontend/src/app/jugar-partida/jugar-partida.component.ts
@@ -178,10 +178,12 @@ export class JugarPartidaComponent implements OnInit {
       if(this.partida.estado!="ganada"&&this.partida.estado!="perdida") {
         try {
           if (this.tablero2D[row][col] == -1) {
-            this.partida = await this.partidaHttpService.cambiarEstadoCasilla(row, col, this.partida.id!, -2).toPromise()
+            this.tablero2D[row][col] = -2;
+            this.partidaHttpService.cambiarEstadoCasilla(row, col, this.partida.id!, -2)
             this.handleTableroUpdate()
           }else if (this.tablero2D[row][col] == -2) {
-            this.partida = await this.partidaHttpService.cambiarEstadoCasilla(row, col, this.partida.id!, -1).toPromise()
+            this.tablero2D[row][col] = -1;
+            this.partidaHttpService.cambiarEstadoCasilla(row, col, this.partida.id!, -1)
             this.handleTableroUpdate()
           }
 
